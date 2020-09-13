132b4e749b775c946ed547b1584569482857d178
SuyamaDaichi <symdit@gmail.com>
2020-09-13 14:27:02 +0900

    郵便番号から住所を取得するサンプルAPI

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,28 @@
 import * as functions from 'firebase-functions';
+import axios from 'axios';
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
-export const helloWorld = functions.https.onRequest((request, response) => {
-    functions.logger.info("Hello logs!", { structuredData: true });
-    response.send("Hello from Firebase!");
+export const getAddress = functions.https.onRequest(async (request, response) => {
+    const baseRequest = axios.create({
+        baseURL: `https://zipcloud.ibsnet.co.jp/api`,
+        responseType: 'json'
+    });
+
+    await baseRequest.get('search', {
+        params: {
+            zipcode: request.query.zipcode
+        }
+    })
+        .then(res => {
+            functions.logger.info((res.data), { structuredData: true });
+            response.send((res.data));
+        })
+        .catch(e => {
+            console.error(e);
+            response.status(500).send({
+                message: 'Error'
+            });
+        });
 });
